9d81c08593ba441b79d72fc4911be9e3ceeecad5
Ruchira_Ruwantha <rruwantha222@gmail.com>
2025-07-26 15:01:02 +0530

    feat: implement the graphic-design page and related components

--- src/pages/Graphic/Graphic.tsx
+++ src/pages/Graphic/Graphic.tsx
@@ -1,7 +1,17 @@
+import ContentComponent from "../../components/common/contentComponent/ContentComponent";
+import DesignComponent from "../../components/common/designcomponent/Design";
+import DesignsTiles from "../../components/common/designNavTiles/designsTiles";
+import { graphicDesignContent } from "../../utils/constants";
+
 const Graphic = () => {
     return (
-        <div>
-            <h2>GRAPHIC</h2>
+        <div className="flex flex-col lg:gap-32 gap-28 mb-12">
+            <DesignComponent
+                title="Graphic Design"
+                desc={<span>We deliver eye-catching branding materials that are <br />tailored to meet your business objectives.</span>}
+            />
+            <ContentComponent designContent={graphicDesignContent} />
+            <DesignsTiles />
         </div>
     );
 }
